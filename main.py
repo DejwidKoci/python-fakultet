89a579dddb85fc01d21f2c98b66f45937f96a7ae
Dejwid <s205915@sggw.edu.pl>
2024-12-31 18:45:14 +0100

    Jakieś zmiany

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-import time
-
-# Liczba sekund od 1 stycznia 1970 roku, czyli tzw. Unix timestamp
-timestamp = time.time()
-
-# Przekształcanie timestamp na lokalny czas
-local_time = time.localtime(timestamp)
-
-# Sformatuj czas w czytelny sposób
-formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
-
-print("Aktualna data i godzina:", formatted_time)
+# Tu będzie appka we Flasku
